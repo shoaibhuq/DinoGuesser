8480b731f1535f96b70952a79727dec6f38085c3
Shoaib Huq <slimsho123@gmail.com>
2023-02-20 00:57:28 -0600

    Initial Commit

---
+++ Dino-Guesser-Lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dino-Guesser-Lab1
+//
+//  Created by Shoaib Huq on 2/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
